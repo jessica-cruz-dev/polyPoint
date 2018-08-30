6fe347d78c8b0d9390d29a284ee206495b93348b
jessica-a-cruz16 <42278346+jessica-a-cruz16@users.noreply.github.com>
2018-08-30 11:15:59 -0500

    Update render_overlap_tool.py

--- render_overlap_tool.py
+++ render_overlap_tool.py
@@ -3,7 +3,7 @@ import glob
 import pandas as pd
 
 # Read in all summary reports
-path1 = r'file_path...'
+path1 = r'J:/temp/jessicac/outputs/*.csv'
 allFiles = glob.glob(path1)
 
 # Isolate the image file names for infograph retrieval
@@ -26,7 +26,7 @@ affected_nid_reports = [y.to_dict('records') for y in affected]
 affected_nid_reports.insert(0, [])
 
 # Read in template
-with open('file_path...') as f:
+with open(J:/temp/jessicac/overlap_render.html') as f:
     tmpl = Template(f.read())
 
 # Render template
@@ -34,6 +34,6 @@ output_template = tmpl.render(nid_list=problem_nids, file_list=file_names,
                               chart_list=chart_values,
                               summary_list=affected_nid_reports)[3:]
 # Generate static webpage
-with open('file_path...', 'w') as f:
+with open('J:/temp/jessicac/overlap_dropdown.html', 'w') as f:
     f.write(output_template)
  
